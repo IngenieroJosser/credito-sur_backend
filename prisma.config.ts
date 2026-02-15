7176a67f6233d7793813df8b82767ff37ebd451e
IngenieroJosser <cordobarivasjosser@gmail.com>
2026-02-14 19:11:03 -0500

    cambios

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,14 +2,14 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'src/prisma/schema.prisma',
 
   datasource: {
     url: process.env.DATABASE_URL,
   },
 
   migrations: {
-    path: 'prisma/migrations',
-    seed: 'ts-node prisma/seed.ts',
+    path: 'src/prisma/migrations',
+    seed: 'ts-node src/prisma/seed.ts',
   },
 });
