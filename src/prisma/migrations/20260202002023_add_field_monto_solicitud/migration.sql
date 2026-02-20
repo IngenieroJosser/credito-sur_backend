cfec4918cfe45176eba18f12c72898dd985e6b2f
IngenieroJosser <cordobarivasjosser@gmail.com>
2026-02-19 21:59:20 -0500

    cambio de migración

--- src/prisma/migrations/20260202002023_add_field_monto_solicitud/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "aprobaciones" ADD COLUMN     "montoSolicitud" DECIMAL(12,2);
--- src/prisma/migrations/20260212140417_add_sidebar_metadata_and_ruta_default/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "permisos" ADD COLUMN     "esNavegable" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN     "icono" VARCHAR(50),
-ADD COLUMN     "orden" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "ruta" VARCHAR(200);
-
--- AlterTable
-ALTER TABLE "roles" ADD COLUMN     "rutaDefault" VARCHAR(100);
