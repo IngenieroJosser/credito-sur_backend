e2bcc10b9ecce49afd80dc4192b51534cc2e06ce
Erick Manuel Moreno Palacios <erickmanuel238@gmail.com>
2026-03-30 11:17:21 -0500

    REDIS

--- src/app.module.ts
+++ src/app.module.ts
@@ -59,7 +59,9 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
       connection: {
         host: process.env.REDIS_HOST || 'localhost',
         port: parseInt(process.env.REDIS_PORT || '6379'),
+        username: process.env.REDIS_USERNAME || undefined,
         password: process.env.REDIS_PASSWORD || undefined,
+        tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
       },
     }),
     MirrorSyncModule,
