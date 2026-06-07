57db28d29a13bf6635c60d24cb21d7cc7908bde9
Erick Manuel Moreno Palacios <erickmanuel238@gmail.com>
2026-06-07 01:03:36 -0500

    rollback

--- src/accounting/accounting.module.ts
+++ src/accounting/accounting.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { AccountingService } from './accounting.service';
 import { AccountingController } from './accounting.controller';
 import { LedgerService } from './ledger.service';
-import { RoutesModule } from '../routes/routes.module';
 
 @Module({
-  imports: [RoutesModule],
   controllers: [AccountingController],
   providers: [AccountingService, LedgerService],
   exports: [LedgerService],
